06ab8e70dc8739bf6daf29243e0ee13244d3b211
Mohamed Salem <4mohamedsalem@gmail.com>
2023-03-11 23:05:42 +0200

    Update Product Code

--- controllers/admin.js
+++ controllers/admin.js
@@ -25,17 +25,15 @@ exports.postAddProduct = (req, res, next) => {
       console.log(err)
     })
 }
-/*
+
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit
   if (!editMode) {
     return res.redirect("/")
   }
   const prodId = req.params.productId
-  req.user.getProducts({ where: { id: prodId } })
-  Product.findByPk(prodId)
-    .then((products) => {
-      const product = products[0]
+  Product.findById(prodId)
+    .then((product) => {
       if (!product) {
         return res.redirect("/")
       }
@@ -48,32 +46,32 @@ exports.getEditProduct = (req, res, next) => {
     })
     .catch((err) => console.log(err))
 }
-/*
+
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId
   const updatedTitle = req.body.title
   const updatedPrice = req.body.price
-  const updatedImageUrl = req.body.imageUrl
   const updatedDesc = req.body.description
-  Product.findByPk(prodId)
-    .then((product) => {
-      product.title = updatedTitle
-      product.price = updatedPrice
-      product.description = updatedDesc
-      product.imageUrl = updatedImageUrl
-      return product.save()
-    })
+  const updatedImageUrl = req.body.imageUrl
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  )
+  product
+    .save()
     .then((result) => {
       console.log("UPDATED PRODUCT!")
       res.redirect("/admin/products")
     })
     .catch((err) => console.log(err))
 }
-*/
-/*
+
 exports.getProducts = (req, res, next) => {
-  req.user
-    .getProducts()
+  Product.fetchAll()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
@@ -83,7 +81,6 @@ exports.getProducts = (req, res, next) => {
     })
     .catch((err) => console.log(err))
 }
-*/
 /*
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId

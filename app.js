185709cf0c65a2dd3f3442113b11d45bf4ff3a45
Mohamed Salem <4mohamedsalem@gmail.com>
2023-03-15 03:43:58 +0200

    Creating the Product Schema & Saving Data Through Mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 
 const errorController = require("./controllers/error")
-const User = require("./models/user")
+// const User = require("./models/user")
 
 const app = express()
 
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
 
-app.use((req, res, next) => {
-  User.findById("640d1d3414b484265d96c86b")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id)
-      next()
-    })
-    .catch((err) => console.log(err))
-})
+// app.use((req, res, next) => {
+//   User.findById("640d1d3414b484265d96c86b")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id)
+//       next()
+//     })
+//     .catch((err) => console.log(err))
+// })
 
 app.use("/admin", adminRoutes)
 app.use(shopRoutes)
@@ -34,7 +34,7 @@ app.use(errorController.get404)
 
 mongoose
   .connect(
-    "mongodb+srv://maxsalem:8bOpKt8LHkTfClzy@cluster0.whgqi1j.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://maxsalem:8bOpKt8LHkTfClzy@cluster0.whgqi1j.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
     app.listen(3000)

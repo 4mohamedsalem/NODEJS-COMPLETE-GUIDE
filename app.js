1eaf68c150197d5879d62f00b067c3d20e154aaa
Mohamed Salem <4mohamedsalem@gmail.com>
2023-03-17 02:37:43 +0200

    Adding and Using a User Model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 
 const errorController = require("./controllers/error")
-// const User = require("./models/user")
+const User = require("./models/user")
 
 const app = express()
 
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
 
-// app.use((req, res, next) => {
-//   User.findById("640d1d3414b484265d96c86b")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id)
-//       next()
-//     })
-//     .catch((err) => console.log(err))
-// })
+app.use((req, res, next) => {
+  User.findById("6413b5ed1eec4114a4872918")
+    .then((user) => {
+      req.user = user
+      next()
+    })
+    .catch((err) => console.log(err))
+})
 
 app.use("/admin", adminRoutes)
 app.use(shopRoutes)
@@ -37,6 +37,18 @@ mongoose
     "mongodb+srv://maxsalem:8bOpKt8LHkTfClzy@cluster0.whgqi1j.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Salem",
+          email: "salem@test.com",
+          cart: {
+            items: [],
+          },
+        })
+        user.save()
+      }
+    })
     app.listen(3000)
   })
   .catch((err) => {

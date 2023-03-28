b34b8fdec86a9487ce1d5fa13303ae6e71fb45a5
Mohamed Salem <4mohamedsalem@gmail.com>
2023-03-28 06:54:31 +0200

    Adding Async Validation & Keeping User Input

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,12 +41,27 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   })
 }
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+    })
+  }
+
   User.findOne({ email })
     .then((user) => {
       if (!user) {
@@ -78,7 +93,7 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email
   const password = req.body.password
-  const confirmPassword = req.body.confirmPassword
+
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     console.log(errors.array())
@@ -86,39 +101,32 @@ exports.postSignup = (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email,
+        password,
+        confirmPassword: req.body.confirmPassword,
+      },
     })
   }
-  User.findOne({ email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash(
-          "error",
-          "E-Mail exists already, please pick a different one."
-        )
-        return res.redirect("/signup")
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email,
-            password: hashedPassword,
-            cart: { items: [] },
-          })
-          return user.save()
-        })
-        .then((result) => {
-          res.redirect("/login")
-          return transporter.sendMail({
-            to: email,
-            from: "shop@node-complete.com",
-            subject: "Signup succeeded!",
-            html: "<h1>You successfully signed up!</h1>",
-          })
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email,
+        password: hashedPassword,
+        cart: { items: [] },
+      })
+      return user.save()
+    })
+    .then((result) => {
+      res.redirect("/login")
+      // return transporter.sendMail({
+      //   to: email,
+      //   from: 'shop@node-complete.com',
+      //   subject: 'Signup succeeded!',
+      //   html: '<h1>You successfully signed up!</h1>'
+      // });
     })
     .catch((err) => {
       console.log(err)
